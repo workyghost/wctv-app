45e4d313a43791505e9b1db6eb7c989eac8d5e7b
WorkyGhost Admin <admin@workyghost.com>
2026-06-14 21:07:25 +0300

    fix(stream): fetch playlists directly from CDN, bypassing the blocked VPS proxy

--- src/App.jsx
+++ src/App.jsx
@@ -44,8 +44,7 @@ const PRESET_CHANNELS = [
 
 // Generates virtual master playlist containing all resolutions for ABR
 const getVirtualMasterPlaylist = (channel, sid) => {
-  const origin = typeof window !== 'undefined' ? window.location.origin : '';
-  const proxyBase = `${origin}/trt-proxy`;
+  const proxyBase = 'https://trt.daioncdn.net';
   if (channel.channelKey === 'trt-1') {
     return `#EXTM3U
 #EXT-X-VERSION:3
@@ -177,8 +176,7 @@ function App() {
 
     let blobUrl = '';
     const sid = generateRandomSid();
-    const origin = typeof window !== 'undefined' ? window.location.origin : '';
-    const proxyBase = `${origin}/trt-proxy`;
+    const proxyBase = 'https://trt.daioncdn.net';
 
     if (activeChannel.isDynamic) {
       // Create virtual master playlist Blob to support Adaptive Bitrate
@@ -326,8 +324,7 @@ function App() {
 
       // 2. SELF-PING CONNECTION CHECK (every 8 seconds)
       if (tickCount % 8 === 0) {
-        const origin = typeof window !== 'undefined' ? window.location.origin : '';
-        const proxyBase = `${origin}/trt-proxy`;
+        const proxyBase = 'https://trt.daioncdn.net';
         const pingUrl = activeChannel.isDynamic
           ? `${proxyBase}/${activeChannel.channelKey}/master_1080p.m3u8?sid=${sid}&app=${APP_GUID}&ce=2`
           : activeChannel.url;
